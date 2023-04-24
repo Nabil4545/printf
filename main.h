81e2c0e3f762c9514c2dfbbf14a41eaa5715bf23
Nabil4545 <nabil.touati25@gmail.com>
2023-04-23 17:35:12 -0700

    update3

--- main.h
+++ main.h
@@ -19,11 +19,11 @@
 #define S_SHORT 1
 
 /**
- * struct fmt - Struct op
- *
- * @fmt: The format.
- * @fn: The function associated.
- */
+* struct fmt - Struct op
+*
+* @fmt: The format.
+* @fn: The function associated.
+*/
 struct fmt
 {
 char fmt;
@@ -32,11 +32,11 @@ int (*fn)(va_list, char[], int, int, int, int);
 
 
 /**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
- * @fm_t: The function associated.
- */
+* typedef struct fmt fmt_t - Struct op
+*
+* @fmt: The format.
+* @fm_t: The function associated.
+*/
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
